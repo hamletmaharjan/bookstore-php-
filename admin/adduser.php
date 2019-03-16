ca6a933326961db4e9199a570c1b1a1d41c8c971
hamlet <maharjansaleem@gmail.com>
2019-03-16 23:39:05 +0545

    added add user feature

---
+++ admin/adduser.php
@@ -0,0 +1,136 @@
+<?php
+include 'layouts/header.php';
+include 'layouts/sidebar.php';
+
+?>
+
+        
+
+         <!--main content start-->
+          <section id="main-content">
+              <section class="wrapper">
+    		  <div class="row">
+    				<div class="col-lg-12">
+    					<h3 class="page-header"><i class="fa fa-file-text-o"></i> Add User </h3>
+    					<ol class="breadcrumb">
+    						<li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
+    						<li><i class="icon_document_alt"></i>Users</li>
+    						<li><i class="fa fa-file-text-o"></i>Add User</li>
+    					</ol>
+    				</div>
+    			</div>
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <section class="panel">
+                              <header class="panel-heading">
+                                User Form
+                              </header>
+                              <div class="panel-body">
+                                  <form class="form-horizontal " method="POST">
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">First Name</label>
+                                          <div class="col-sm-10">
+                                              <input type="text" name="first_name" class="form-control">
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Last Name</label>
+                                          <div class="col-sm-10">
+                                              <input type="text" name="last_name" class="form-control">
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Email</label>
+                                          <div class="col-sm-10">
+                                              <input type="email" name="email" class="form-control">
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Passowrd</label>
+                                          <div class="col-sm-10">
+                                              <input type="password" name="password" class="form-control">
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Contact</label>
+                                          <div class="col-sm-10">
+                                              <input type="text" name="contact" class="form-control">
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Role</label>
+                                          <div class="col-sm-10">
+                                            <select class="form-control m-bot15">
+                                              <option value="superadmin">Super Admin</option>
+                                              <option value="admin">Admin</option>
+                                            </select>
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <label class="col-sm-2 control-label">Status</label>
+                                          <div class="col-sm-10">
+                                            <select class="form-control m-bot15" name="status">
+                                              <option value="active">Active</option>
+                                              <option value="inactive">Innactive</option>
+                                          </select>
+                                          </div>
+                                      </div>
+                                      <div class="form-group">
+                                          <button type="submit" class="btn btn-primary">Submit</button>
+                                      </div>
+                                  </form>
+                              </div>
+                          </section>
+                          
+                      </div>
+                  </div>
+                 
+              </section>
+          </section>
+          <!--main content end-->
+          <div class="text-right">
+            <div class="credits">
+                <!-- 
+                    All the links in the footer should remain intact. 
+                    You can delete the links only if you purchased the pro version.
+                    Licensing information: https://bootstrapmade.com/license/
+                    Purchase the pro version form: https://bootstrapmade.com/buy/?theme=NiceAdmin
+                -->
+                <a href="https://bootstrapmade.com/free-business-bootstrap-themes-website-templates/">Business Bootstrap Themes</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+      </section>
+      <!-- container section end -->
+        <!-- javascripts -->
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- nice scroll -->
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+
+        <!-- jquery ui -->
+        <script src="js/jquery-ui-1.9.2.custom.min.js"></script>
+
+        <!--custom checkbox & radio-->
+        <script type="text/javascript" src="js/ga.js"></script>
+        <!--custom switch-->
+        <script src="js/bootstrap-switch.js"></script>
+        <!--custom tagsinput-->
+        <script src="js/jquery.tagsinput.js"></script>
+        
+        <!-- colorpicker -->
+       
+        <!-- bootstrap-wysiwyg -->
+        <script src="js/jquery.hotkeys.js"></script>
+        <script src="js/bootstrap-wysiwyg.js"></script>
+        <script src="js/bootstrap-wysiwyg-custom.js"></script>
+        <!-- ck editor -->
+        <script type="text/javascript" src="assets/ckeditor/ckeditor.js"></script>
+        <!-- custom form component script for this page-->
+        <script src="js/form-component.js"></script>
+        <!-- custome script for all page -->
+        <script src="js/scripts.js"></script>
+
+
+      </body>
+    </html>
