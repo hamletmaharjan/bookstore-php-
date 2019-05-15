8f053790d6acec1221294e54a9b17b2b0000dd6e
hamlet <maharjansaleem@gmail.com>
2019-05-15 21:49:52 +0545

    trying to add frontend

---
+++ front/test.php
@@ -0,0 +1,18 @@
+<?php
+
+include '../config/call.php';
+
+
+if($conn){
+    echo "connected";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hams Book Store</title>
+</head>
+<body>
+
+</body>
+</html>
